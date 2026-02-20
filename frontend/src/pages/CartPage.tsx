790f248756a18316fabdd0c8d76686d281263804
Mano <m@mano.com>
2026-02-21 01:10:53 +0400

    Cart Page Layout

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,29 +1,49 @@
 import { Box, Typography } from "@mui/material";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import Container from "@mui/material/Container";
-import { use, useEffect, useState } from "react";
-import { BASE_URL } from "../constants/baseUrl";
-import { useAuth } from "../context/Auth/AuthContext";
 import { useCart } from "../context/Cart/CartContext";
+
 
 const CartPage = () => {
 
-    const {token} = useAuth();
     const {cartItems, totalAmount} = useCart();
-    const [error, setError] = useState('');
 
 
     return (
-        <Container sx={{ mt: 2 }}>
-
-            <Typography variant="h4">My Cart</Typography>
-                {cartItems.map((item) => (
-                    <Box key={item.productId}>
-                     {item.title} - {item.quantity} x ${item.unitPrice}
-                    </Box>
-                ))}
-            <Typography variant="h5">Total: ${totalAmount}</Typography>
-            {error && <Typography color="error">{error}</Typography>}
-        </Container>
+      <Container fixed sx={{ mt: 2 }}>
+        <Typography variant="h4">My Cart</Typography>
+        {cartItems.map((item) => (
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            flexDirection="row"
+            key={item.productId}
+            sx={{ mt: 2, p: 2, border: "1px solid #ccc", borderRadius: "4px" }}
+          >
+            <Box display="flex" flexDirection="row" gap={2} alignItems="center">
+              <img
+                src={item.image}
+                style={{ width: "100px", height: "100px", objectFit: "cover" }}
+              />
+              <Box>
+                <Typography variant="h6"> {item.title} </Typography>
+                <Typography>
+                  {" "}
+                  {item.quantity} x ${item.unitPrice}{" "}
+                </Typography>
+                <Button size="small">Remove Item</Button>
+              </Box>
+            </Box>
+            <ButtonGroup variant="contained" aria-label="Basic button group">
+              <Button>-</Button>
+              <Button>+</Button>
+            </ButtonGroup>
+          </Box>
+        ))}
+        <Box sx={{ mt: 2 }}><Typography variant="h4">Total: ${totalAmount.toFixed(2)}</Typography></Box>
+      </Container>
     );
 };
 export default CartPage;
